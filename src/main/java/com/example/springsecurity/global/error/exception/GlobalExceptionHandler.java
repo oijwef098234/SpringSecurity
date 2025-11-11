6311123fc582fe8fdb94e1a0f4b86a313f6afbbb
oijwef098234 <oijwef098234@gmail.com>
2025-11-12 00:11:41 +0900

    2025.11.11

--- src/main/java/com/example/springsecurity/global/error/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/springsecurity/global/error/exception/GlobalExceptionHandler.java
@@ -10,14 +10,14 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleSecurityException(SpringSecurityException e) {
         return ResponseEntity
                 .status(e.getErrorCode().getStatusCode())  // 네 ErrorCode에 상태코드 있으면 그거 쓰고
-                .body(new ErrorResponse(e.getErrorCode().name(), e.getMessage()));
+                .body(new ErrorResponse(e.getErrorCode().getStatusCode(), e.getErrorCode().getErrorMessage()));
     }
 
     static class ErrorResponse {
-        public final String code;
+        public final int code;
         public final String message;
-        public ErrorResponse(String code, String message) {
-            this.code = code;
+        public ErrorResponse(int statusCode, String message) {
+            this.code = statusCode;
             this.message = message;
         }
     }
